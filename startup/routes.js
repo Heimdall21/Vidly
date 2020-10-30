261206ac2a410cba7b0d246c099ef18ab7a6d4e7
Jae Jeon <jae.jeon11@gmail.com>
2020-10-31 10:52:10 +1100

    about to start the final module of Deployment

--- startup/routes.js
+++ startup/routes.js
@@ -6,7 +6,9 @@ const customers = require('../routes/customers');
 const movies = require('../routes/movies');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
+const returns = require('../routes/returns');
 const error = require('../middleware/error');
+
 
 module.exports = function(app) {
     app.use(express.json());
@@ -17,5 +19,6 @@ module.exports = function(app) {
     app.use('/api/movies', movies);
     app.use('/api/users', users);
     app.use('/api/auth', auth);
+    app.use('/api/returns', returns);
     app.use(error);
 }
