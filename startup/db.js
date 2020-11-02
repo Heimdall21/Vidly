1323f3fff1cd99c0123af25a28da4690a9874b54
Jae Jeon <jae.jeon11@gmail.com>
2020-11-02 22:39:30 +1100

    completed the course but there is an error at the end that I have not fixed

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,19 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://1234:1234@cluster0.w6z7c.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+/* module.exports = function () {
+    const client = new MongoClient(uri, { useNewUrlParser: true});
+
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        client.close();
+    })
+} */
+
+
 module.exports = function() {
     // MongoDB stuff
     // 1. Connect to database
@@ -11,5 +24,9 @@ module.exports = function() {
         .then(() => winston.info(`Connected to ${db}`))
         //.catch(err => console.error('Could not connect to MongoDB Vidly: ', err));
 }
+
+
+
+
 
 
